eec07755f1959e15683a1c17bba8427a731bd03d
brendon guedes <brendon.guedes@corebiz.ag>
2020-10-13 22:08:45 -0300

    finalizado

--- com/kuma/test/test_operacoes.py
+++ com/kuma/test/test_operacoes.py
@@ -1,7 +1,6 @@
-import pytest
-from com.kuma.operacoes import Operacoes
+def func(x):
+    return x + 1
 
 
-def test_soma():
-    operacoes = Operacoes()
-    assert operacoes.soma([1.5]) == 6, "Should be 6"
+def test_answer():
+    assert func(3) == 4
